9a0f8427ccb7c1525c29b821b8ef7b7c8ddce3fd
softacoder <113185323+softacoder@users.noreply.github.com>
2024-05-23 03:46:05 +0100

    commit node implementation

--- Main.py
+++ Main.py
@@ -6,40 +6,11 @@ from Blockchain import Blockchain
 import pprint
 from BlockchainUtils import BlockchainUtils
 from AccountModel import AccountModel
+from Node import Node
 
 if __name__ == '__main__':
-   
-    blockchain = Blockchain()
-    pool = TransactionPool()
-
-    alice = Wallet()
-    bob = Wallet()
-    exchange = Wallet()
-
-    exchangeTransaction = exchange.createTransaction(
-        alice.publicKeyString(), 10, 'EXCHANGE')
+   node = Node()
+   print(node.blockchain)
+   print(node.transactions)
+   print(node.wallet)
     
-    if not pool.transactionExists(exchangeTransaction):
-        pool.addTransaction(exchangeTransaction)
-
-    coveredTransaction = blockchain.getCoveredTransactionSet(pool.transactions)
-    lastHash = BlockchainUtils.hash(blockchain.blocks[-1].payload()).hexdigest()
-    blockCount = blockchain.blocks[-1].blockCount + 1
-    blockOne = forger.createBlock(coveredTransaction, lastHash, blockCount)
-    blockchain.addBlock(blockOne)
-    pool.removeFromPool(blockOne.transactions)
-
-    #alice wants to spend 5 tokens to bob
-    transaction = alice.createTransaction(bob.publicKeySTring(), 5, 'TRANSFER')
-
-    if not pool.transactionExists(transaction):
-        pool.addTransaction(transaction)
-
-    coveredTransaction = blockchain.getCoveredTransactionSet(pool.transactions)
-    lastHash = BlockchainUtils.hash(blockchain.blocks[-1].payload()).hexdigest()
-    blockCount = blockchain.blocks[-1].blockCount + 1
-    blockTwo = forger.createBlock(coveredTransaction, lastHash, blockCount)
-    blockchain.addBlock(blockTwo)
-    pool.removeFromPool(blockTwo.transactions)
-
-    pprint.pprint(blockchain.toJson())
