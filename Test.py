2e6eb0cabcd23c5cac092fb3f15c612dea7aa6e5
softacoder <113185323+softacoder@users.noreply.github.com>
2024-05-24 19:06:51 +0100

    commit selecting forger

--- Test.py
+++ Test.py
@@ -1,13 +1,29 @@
 from ProofOfStake import ProofOfStake
 from Lot import Lot
+import string
+import random
+
+
+def getRandomString(length):
+    letters = string.ascii_lowercase
+    resultString = ''.join(random.choice(letters) for i in range(length))
+    return resultString
+
 
 if __name__ == '__main__':
-    '''pos = ProofOfStake()
-    pos.update('bob', 10)
+    pos = ProofOfStake()
+    pos.update('bob', 100)
     pos.update('alice', 100)
-    print(pos.get('bob'))
-    print(pos.get('alice'))
-    print(pos.get('jack')) '''
 
-    lot = Lot('bob', 1, 'lastHash')
-    print(lot.lotHash())
+    bobWins = 0
+    aliceWins = 0
+
+    for i in range(100):
+        forger = pos.forger(getRandomString(i))
+        if forger == 'bob':
+            bobWins += 1
+        elif forger == 'alice':
+            aliceWins += 1
+
+    print('Bob won: ' + str(bobWins) + ' times')
+    print('Alice won: ' + str(aliceWins) + ' times')
