93f390763d8c8b6ad06b87124f4d537fc4942a6c
softacoder <113185323+softacoder@users.noreply.github.com>
2024-05-25 19:10:10 +0100

    commit proof of stake based blockchain prototype

--- Interaction.py
+++ Interaction.py
@@ -2,13 +2,26 @@ from Wallet import Wallet
 from BlockchainUtils import BlockchainUtils
 import requests
 
-if __name__ == '__main__':
-    bob = Wallet()
-    exchange = Wallet()
-
-    transaction = exchange.createTransaction(alice.publicKeyString(), 10, 'EXCHANGE')
-
+def postTransaction(sender, receiver, amount, type):
+    transaction = exchange.createTransaction(receiver.publicKeyString(), amount, type)
     url = 'http://localhost:5000/transaction'
     package = {'transaction': BlockchainUtils.encode(transaction)}
     request = requests.post(url, json=package)
-    print(request.text)
+    
+
+if __name__ == '__main__':
+    bob = Wallet()
+    alice = Wallet()
+    alice.fromKey('keys/stakerPrivateKey.pem')
+    exchange = Wallet()
+
+    postTransaction(exchange, alice, 100, 'EXCHANGE')
+    postTransaction(exchange, bob, 100, 'EXCHANGE')
+    postTransaction(exchange, bob, 10, 'EXCHANGE')
+    
+    postTransaction(alice, alice, 25, 'STAKE')
+    postTransaction(alice, bob, 1, 'TRANSFER')
+    postTransaction(alice, bob, 1, 'TRANSFER')
+
+
+    
